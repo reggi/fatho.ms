927927827eca6f12ef57aa3742d4e179c961df09
reggi <thomas@reggi.com>
2013-01-01 15:46:24 -0500

    resize, single css & js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,15 @@
 var stars = function(){
     $(".multiuniverse").stars({
         count:100,
-        speed:100000,
+        speed:100000
     },{
         count:25,
-        speed:150000,
+        speed:150000
     });
 };
 
 $(document).ready(function(){
     stars();
-
-
-
 });
 
 $(window).on("debouncedresize", function( event ) {
